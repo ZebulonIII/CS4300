197b461cf5e4ce77d4d9e0bb273e381e1d1c5302
Samuel Carlson <tritium99@Hotmail.com>
2019-08-13 11:44:39 -0600

    Update Face.h

--- Face.h
+++ Face.h
@@ -5,17 +5,9 @@ private
 public:
   const int SIZE = 9;
   
-  Face(Color color) {
-    for (int i = 0; i < SIZE; i++)
-      colors[i] = color;
-  }
-  Face(Color colors[SIZE]) {
-    for (int i = 0; i < SIZE; i++)
-      this->colors[i] = colors[i];
-  }
-  ~Face() {}
+  Face(Color color);
+  Face(Color colors[SIZE]);
+  ~Face();
   
-  Color& operator[](int i) {
-    return colors[i];
-  }
+  Color& operator[](int i);
 };
